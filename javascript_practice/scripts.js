dc79ea5f41db0e1650ba5c25795514ad2a1d4664
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-01-22 15:45:49 +0900

    2024/01/22

---
+++ javascript_practice/scripts.js
@@ -0,0 +1,19 @@
+// alert("Hello, world!");
+/*
+이것은 모두 주석이다
+console.log(10);
+이것은 모두 실행되지 않는다!
+console.log(99);
+*/
+
+// console.log("seoul");
+// console.log(40);
+
+// console.log(Math.random());
+// var a = Math.random() * 50;
+// console.log(Math.floor(a));
+
+const mbti = "INTP";
+console.log(`내 mbti는 ${mbti}이다.`);
+
+console.log(typeof mbti);
